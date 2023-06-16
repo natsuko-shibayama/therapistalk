8ec5d55dc4b3be25143eaf18f62b7dbba2ea915e
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-16 15:28:17 +0900

    talksテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :talks
+
   validates :nickname, presence: true, length: { maximum: 40 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :job, presence: true
